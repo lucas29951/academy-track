f9e2774975343fec0308761139642370a5f5fc7b
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-23 20:19:13 -0300

    App Module actualizado - MateriaService y FormsModule agregados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { MateriasService } from './services/materias.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,9 +15,10 @@ import { PrincipalComponent } from './principal/principal.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [MateriasService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
