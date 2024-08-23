9e2ef80ff139a30ec1865d36a3c6b2145cc561c0
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-23 20:56:04 -0300

    Ruta agregada para el seguimiento de las materias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './principal/principal.component';
+import { SeguimientoComponent } from './seguimiento/seguimiento.component';
 
 const routes: Routes = [
   { path: '', component: PrincipalComponent },
+  { path: 'seguimiento', component: SeguimientoComponent },
 ];
 
 @NgModule({
