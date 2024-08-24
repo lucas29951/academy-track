ba58079ee255f2713df0228a38b0f6e622977d11
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-24 12:23:33 -0300

    Nuevo componente agregado - Aprobadas

---
+++ src/app/aprobadas/aprobadas.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AprobadasComponent } from './aprobadas.component';
+
+describe('AprobadasComponent', () => {
+  let component: AprobadasComponent;
+  let fixture: ComponentFixture<AprobadasComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AprobadasComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AprobadasComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/aprobadas/aprobadas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aprobadas',
+  templateUrl: './aprobadas.component.html',
+  styleUrls: ['./aprobadas.component.css']
+})
+export class AprobadasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
