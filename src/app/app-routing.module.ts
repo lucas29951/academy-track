11f5f6d5fc63cc954f377a496af8b65516bf2f94
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-24 12:50:15 -0300

    Ruta configurada para la vista de materias aprobadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './principal/principal.component';
 import { SeguimientoComponent } from './seguimiento/seguimiento.component';
+import { AprobadasComponent } from './aprobadas/aprobadas.component';
 
 const routes: Routes = [
   { path: '', component: PrincipalComponent },
   { path: 'seguimiento', component: SeguimientoComponent },
+  { path: 'aprobadas', component: AprobadasComponent },
 ];
 
 @NgModule({
