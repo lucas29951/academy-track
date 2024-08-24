05830084c4ce8b08893c2ea753d25129a2638fe8
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-24 12:48:48 -0300

    Logica implementada para filtrar las materias aprobadas y el progreso academico

--- src/app/aprobadas/aprobadas.component.ts
+++ src/app/aprobadas/aprobadas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MateriasService } from '../services/materias.service';
 
 @Component({
   selector: 'app-aprobadas',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aprobadas.component.css']
 })
 export class AprobadasComponent implements OnInit {
+  materias: any[] = [];
+  materiasAprobadas: any[] = [];
+  progreso: number = 0;
+  aprobadas: number = 0;
+  totalMaterias: number = 0;
 
-  constructor() { }
+  constructor(private materiasService: MateriasService) { }
 
   ngOnInit(): void {
   }
 
+  cargarMaterias() {
+    const data = this.materiasService.getMateriasData();
+    if (data.length > 0) {
+      this.materias = data;
+    }
+  }
+
+  filtrarAprobadas() {
+    this.materiasAprobadas = this.materias.filter(materia => materia.estado === 'aprobada');
+  }
+
+  calcularProgreso() {
+    this.totalMaterias = this.materias.length;
+    this.aprobadas = this.materiasAprobadas.length;
+    this.progreso = Math.round((this.aprobadas / this.totalMaterias) * 100);
+  }
 }
