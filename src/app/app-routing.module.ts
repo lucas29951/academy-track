94b0960f68fe979e27bfd6e67d657053b5f9cd37
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-25 18:21:02 -0300

    Ruta agregada para la vista de Historia Academica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { PrincipalComponent } from './principal/principal.component';
 import { SeguimientoComponent } from './seguimiento/seguimiento.component';
 import { AprobadasComponent } from './aprobadas/aprobadas.component';
 import { AyudaComponent } from './ayuda/ayuda.component';
+import { HistoriaComponent } from './historia/historia.component';
 
 const routes: Routes = [
   { path: '', component: PrincipalComponent },
   { path: 'seguimiento', component: SeguimientoComponent },
+  { path: 'historia', component: HistoriaComponent },
   { path: 'aprobadas', component: AprobadasComponent },
   { path: 'ayuda', component: AyudaComponent },
 ];
