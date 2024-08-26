17e65fea611c97c5be9d0689f82ed2873c877249
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-25 22:08:37 -0300

    Logica de carga y determinacion de las materias implementada

--- src/app/historia/historia.component.ts
+++ src/app/historia/historia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MateriasService } from '../services/materias.service';
 
 @Component({
   selector: 'app-historia',
@@ -6,10 +7,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./historia.component.css']
 })
 export class HistoriaComponent implements OnInit {
+  materias: any[] = [];
+  materiasAprobadas: any[] = [];
+  materiasReprobadas: any[] = [];
+  materiasEnCurso: any[] = [];
+  materiasRegulares: any[] = [];
+  totalMaterias: number = 0;
+  porcentajeAvance: number = 0;
+  promedioGeneral: number = 0;
+  promedioConAplazos: number = 0;
+  aniosCursados: any[] = [];
 
-  constructor() { }
+  constructor(private materiasService: MateriasService) { }
 
   ngOnInit(): void {
+    this.cargarMaterias();
+    this.calcularProgresoYPromedio();
+    this.organizarMateriasPorAnio();
   }
 
+  cargarMaterias() {
+    const data = this.materiasService.getMateriasData();
+    if (data.length > 0) {
+      this.materias = data;
+      this.totalMaterias = data.length;
+    }
+  }
+
+  calcularProgresoYPromedio() {
+    this.materiasAprobadas = this.materias.filter(m => m.estado === 'aprobada');
+    this.materiasReprobadas = this.materias.filter(m => m.estado === 'reprobada');
+    this.materiasEnCurso = this.materias.filter(m => m.estado === 'en curso');
+    this.materiasRegulares = this.materias.filter(m => m.estado === 'regular');
+    const totalNotas = this.materiasAprobadas.reduce((sum, m) => sum + (m.notaFinal || 0), 0);
+    this.promedioGeneral = this.materiasAprobadas.length ? Math.round(totalNotas / this.materiasAprobadas.length) : 0;
+    const totalNotasAplazos = this.materias.reduce((sum, m) => sum + (m.notaFinal || 0), 0);
+    this.promedioConAplazos = this.materias.length ? Math.round(totalNotasAplazos / this.materias.length) : 0;
+    this.porcentajeAvance = Math.round((this.materiasAprobadas.length / this.totalMaterias) * 100);
+  }
+
+  organizarMateriasPorAnio() {
+    const grouped = this.materias.reduce((acc, materia) => {
+      const anio = materia.anio || 'Sin Año';
+      if (!acc[anio]) acc[anio] = { anio, materias: [], open: false };
+      acc[anio].materias.push(materia);
+      return acc;
+    }, {});
+    this.aniosCursados = Object.values(grouped);
+  }
+
+  toggleYear(anio: any) {
+    anio.open = !anio.open;
+  }
+
+  getClassForEstado(estado: string) {
+    switch (estado) {
+      case 'aprobada': return 'has-text-success';
+      case 'en curso': return 'has-text-warning';
+      case 'regular': return 'has-text-info';
+      default: return '';
+    }
+  }
 }
